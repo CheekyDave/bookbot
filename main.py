0a4c4589f0d7c3fccb48d37b8b94b7ad6dfc622e
Dave Mason <Dave.Mason@faculty.ai>
2024-02-02 15:10:24 +0000

    updated project with analysis code

---
+++ main.py
@@ -0,0 +1,51 @@
+def main():
+    path_to_file = "books/frankenstein.txt"
+    text = get_book_text(path_to_file)
+    word_count = count_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+main()
